902433f3c51b575f41865366d802dfc85ad556aa
Alejo-rey <771@holbertonschool.com>
2019-08-13 22:22:31 +0000

    [UPDATE] update database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Create a database if not exist
+-- Create a database if not exist.
 CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
