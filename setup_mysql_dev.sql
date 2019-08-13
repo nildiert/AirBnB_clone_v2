d42cdd7f8fdb981a6449f89c6db68d85bcfea703
Alejo-rey <771@holbertonschool.com>
2019-08-13 22:07:27 +0000

    [FIX] Commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- Create a database if not exist
--- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
