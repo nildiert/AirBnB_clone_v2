07088e444d37d3370cb731bf80eb04fb883ce74a
Alejo-rey <771@holbertonschool.com>
2019-08-16 01:09:12 +0000

    [FEATURE] creating the relationship Many-To-Many between Place and Amenity

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,12 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if 'id' not in kwargs.values():
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs.values():
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs.values():
+                self.updated_at = datetime.now()
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
