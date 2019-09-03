e2c91f807228a0618c494903e19984922a3612f2
nildiert <niljordan23@gmail.com>
2019-09-03 17:07:47 -0500

    [FEATURE] Added file 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" First route Flask """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBTN:
+    return 'Hello HBTN!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
