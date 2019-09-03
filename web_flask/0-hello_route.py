ebc08ccc5f887912940367e03572fa0adf21cf8a
nildiert <niljordan23@gmail.com>
2019-09-03 17:03:16 -0500

    [FIX] Fixed betty style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,15 @@
 """ Flask first app """
 from flask import Flask
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_HBTN():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
