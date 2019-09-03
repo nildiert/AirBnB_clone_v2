c9475479e2d8e8d6c8739d5cef43c41e7aab4540
nildiert <niljordan23@gmail.com>
2019-09-03 18:08:59 -0500

    [FEATURE] Added file 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" First route Flask """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBTN():
+    return 'Hello HBTN!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_function(text):
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_function(text="is_cool"):
+    return 'Python ' + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
