8f60d8d8030876f30463d4a9762a266e0e6aa90a
nildiert <niljordan23@gmail.com>
2019-09-03 19:41:36 -0500

    [FEATURE] Added file 6 and template 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" First route Flask """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBTN():
+    return 'Hello HBTN!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_function(text):
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_function(text="is_cool"):
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def number_function(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    if (n % 2) == 0:
+        text = 'even'
+    else:
+        text = 'odd'
+    context = {
+        'n': n,
+        'text': text
+    }
+    return render_template('6-number_odd_or_even.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
